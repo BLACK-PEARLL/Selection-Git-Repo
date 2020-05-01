d673bc3900a751abb99ba72ddc22a1fafbe51b42
BLACK-PEARLL <60192561+BLACK-PEARLL@users.noreply.github.com>
2020-05-01 22:19:15 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,13 @@
+alert("STAY HOME! STAY SAFE!");
+
+function togglehide()
+{
+    let btn=document.getElementById('btn');
+    let para=document.getElementById('para');
+    if(para.style.display!='none'){
+    para.style.display='none';
+    }
+    else{
+        para.style.display='block';
+    }
+}
